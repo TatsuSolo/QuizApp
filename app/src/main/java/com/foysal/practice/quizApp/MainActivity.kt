29db1cf08291ac09210ea9247b4d6d525a5ebe91
TatsuSolo <1730150@iub.edu.bd>
2022-11-06 20:43:53 +0600

    11-6-22

--- app/src/main/java/com/foysal/practice/quizApp/MainActivity.kt
+++ app/src/main/java/com/foysal/practice/quizApp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.foysal.practice.quizApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //(fix it)window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
     }
 }
