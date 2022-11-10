ec5da47267ddb993dd7803fb39020080e80a0310
TatsuSolo <1730150@iub.edu.bd>
2022-11-10 17:48:51 +0600

    11-9-22

--- app/src/main/java/com/foysal/practice/quizApp/QuizQuestionActivity.kt
+++ app/src/main/java/com/foysal/practice/quizApp/QuizQuestionActivity.kt
@@ -19,14 +19,8 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
     private var mQuestionList: ArrayList<Question>? = null
     private var mSelectedOptionPosition: Int = 0
 
-    var progressBar = findViewById<ProgressBar>(R.id.progress_bar)
-    var tvProgress = findViewById<TextView>(R.id.tv_progress)
-    var tvQuestion = findViewById<TextView>(R.id.tv_question)
-    var ivImage = findViewById<ImageView>(R.id.iv_image)
-    var tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
-    var tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
-    var tvOptionThree = findViewById<TextView>(R.id.tv_option_four)
-    var tvOptionFour = findViewById<TextView>(R.id.tv_option_three)
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +29,10 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
         mQuestionList = Constants.getQuestions()
         //Log.i("Questions Size", "${questionsList.size}")
         setQuestion()
-
+        var tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        var tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        var tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        var tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
         tvOptionOne.setOnClickListener(this)
         tvOptionTwo.setOnClickListener(this)
         tvOptionThree.setOnClickListener(this)
@@ -43,6 +40,10 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
 
     }
     override fun onClick(v: View?) {
+        var tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        var tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        var tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        var tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
         when(v?.id){
             R.id.tv_option_one -> {
                 selectedOptionView(tvOptionOne, selectedOptionNumber = 1)
@@ -62,12 +63,20 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
         val questions = mQuestionList!!.get(mCurrentPosition-1)
 
         defaultOptionView()
+        var progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+        var tvProgress = findViewById<TextView>(R.id.tv_progress)
+        var tvQuestion = findViewById<TextView>(R.id.tv_question)
+        var ivImage = findViewById<ImageView>(R.id.iv_image)
 
         progressBar.progress = mCurrentPosition
         tvProgress.text = "$mCurrentPosition" + "/" + "" + progressBar.max
 
         tvQuestion.text = questions.question
         ivImage.setImageResource(questions.image)
+        var tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        var tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        var tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        var tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
 
         tvOptionOne.text = questions.optionOne
         tvOptionTwo.text = questions.optionTwo
@@ -86,6 +95,10 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
         )
     }
     private fun defaultOptionView(){
+        var tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        var tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        var tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        var tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
         val options = ArrayList<TextView>()
         options.add(0, tvOptionOne)
         options.add(1, tvOptionTwo)
